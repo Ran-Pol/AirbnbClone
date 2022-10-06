14db0a76c79be0df219f9c4169086a6156a2af1d
Ran Pol <rnd809@gmail.com>
2022-10-06 16:14:07 -0400

    Delete the individuals images and replace it with a collage. Futher modify the extra css that is not needed ater the image removal.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css'
 import Nav from './components/Nav'
-import Header from './components/Header'
+import Section from './components/Section'
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Header />
+      <Section />
     </div>
   )
 }
---
+++ src/components/Section.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import photo from '../images/Group7.png'
+
+
+const Section = () => {
+  return (
+    <section className="Header-Intro">
+      <div>
+      <img src={photo} className="header--img img22" alt="La foto" />
+      </div>
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  )
+}
+
+export default Section
